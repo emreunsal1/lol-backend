97b394e81631a446c363b235e426da5f9d174252
Emre Ünsal <unsalemre111@gmail.com>
2022-02-08 18:38:41 +0300

    add lol api interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,26 @@
+export interface IUserInfo{
+  id: string;
+  accountId: string;
+  puuid: string;
+  name: string;
+  profileIconId: number;
+  revisionDate: number;
+  summonerLevel: number;
+}
+
+export interface ILeagueDetail {
+  leagueId: string;
+  queueType: string;
+  tier: string;
+  rank: string;
+  summonerId: string;
+  summonerName: string;
+  leaguePoints: number;
+  wins: number;
+  losses: number;
+  veteran: boolean;
+  inactive: boolean;
+  freshBlood: boolean;
+  hotStreak: boolean;
+}
+
