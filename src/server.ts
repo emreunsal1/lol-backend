d5d84283fb0a08a451d8745ce90f14feff0ce165
Emre Ünsal <unsalemre111@gmail.com>
2022-02-11 16:25:08 +0300

    add swagger json

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 import indexRouter from './routes';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', indexRouter);
+
+// swagger
+app.get('/', (req, res) => {
+  res.redirect('/swagger');
+});
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
