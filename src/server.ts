846bc8154f820b205584e48adba7dba34d2f3e40
Emre Ünsal <unsalemre111@gmail.com>
2022-02-11 14:29:01 +0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import indexRouter from './routes';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api', indexRouter);
 
